a22b3aecdf609c38f1192375889df78a09d42261
urenami <urenami@yahoo.com>
2024-01-30 16:24:55 -0500

    updated settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -46,7 +46,6 @@ ROOT_URLCONF = "recipe_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, 'templates')],
         "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
