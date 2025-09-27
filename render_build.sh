4270912847174f0d5f971b67289d2f820a01831d
urenami <122377715+urenami@users.noreply.github.com>
2025-09-27 17:11:26 -0400

    Clean up render_build.sh (remove auto superuser code)

--- render_build.sh
+++ render_build.sh
@@ -4,11 +4,3 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py migrate
 python manage.py collectstatic --noinput
-
-# Force create admin superuser if not exists
-python manage.py shell <<EOF
-from django.contrib.auth import get_user_model
-User = get_user_model()
-if not User.objects.filter(username="admin").exists():
-    User.objects.create_superuser("admin", "admin@example.com", "adminpassword123")
-EOF
