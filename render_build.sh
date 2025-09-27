6aececa74e064adbbec6541ab94c7a581a3e920b
urenami <122377715+urenami@users.noreply.github.com>
2025-09-27 17:18:19 -0400

    Add auto-create superuser to render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
+# Exit on error
 set -o errexit
 
+# Install dependencies
 pip install -r requirements.txt
+
+# Run migrations
 python manage.py migrate
+
+# Collect static files
 python manage.py collectstatic --noinput
+
+# Create default admin if it doesn't exist
+echo "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@example.com', 'adminpassword123')
+" | python manage.py shell
